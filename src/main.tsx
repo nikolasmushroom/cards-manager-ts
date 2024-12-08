808f494f339dc11b6ecc76b94d81109701bf7484
nikolasmushroom <dashagribok0905@gmail.com>
2024-12-08 15:55:51 +0300

    feat: add styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/700.css'
+
+import './styles/index.scss'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
