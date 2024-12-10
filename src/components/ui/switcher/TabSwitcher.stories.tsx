348410ab72e6e52213a89967e2bf87d457b8479c
nikolasmushroom <dashagribok0905@gmail.com>
2024-12-10 23:06:07 +0300

    feat: added TabSwitcher component

---
+++ src/components/ui/switcher/TabSwitcher.stories.tsx
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { TabSwitcher } from './TabSwitcher.tsx'
+
+const meta = {
+  component: TabSwitcher,
+  tags: ['autodocs'],
+  title: 'Components/UI/TabSwitcher',
+  argTypes: {
+    onChange: () => {},
+  },
+} satisfies Meta<typeof TabSwitcher>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    tabs: [
+      { value: '1', children: 'TestSwitch1' },
+      { value: '2', children: 'TestSwitch2' },
+    ],
+    disabled: false,
+    defaultValue: '1',
+  },
+}
