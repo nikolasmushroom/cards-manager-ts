3d3fd9f7ea5992b7f191bed200413e002a801087
nikolasmushroom <dashagribok0905@gmail.com>
2024-12-10 23:05:50 +0300

    feat: added TextField component

---
+++ src/components/ui/textfield/TextField.stories.tsx
@@ -0,0 +1,69 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { TextField } from './TextField.tsx'
+
+const meta = {
+  component: TextField,
+  tags: ['autodocs'],
+  title: 'Components/UI/TextField',
+  argTypes: { onChange: { action: 'text changes' } },
+} satisfies Meta<typeof TextField>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const DefaultTextField: Story = {
+  args: {
+    type: 'text',
+    label: 'Input',
+    placeholder: 'Input',
+    disabled: true,
+  },
+}
+export const TextFieldWithError: Story = {
+  args: {
+    type: 'text',
+    label: 'Input',
+    placeholder: 'Input',
+    errorMessage: 'Error message will be here',
+    disabled: true,
+  },
+}
+
+export const TextFieldPassword: Story = {
+  args: {
+    type: 'password',
+    label: 'Input',
+    placeholder: 'Password',
+  },
+}
+
+export const TextFieldPasswordWithError: Story = {
+  args: {
+    type: 'password',
+    label: 'Input',
+    placeholder: 'Password',
+    errorMessage: 'Error message will be here',
+    disabled: true,
+  },
+}
+
+export const TextFieldSearch: Story = {
+  args: {
+    type: 'search',
+    placeholder: 'Input search',
+    search: true,
+    disabled: true,
+  },
+}
+
+export const TextfieldSearchWithError: Story = {
+  args: {
+    type: 'search',
+    placeholder: 'Input search',
+    errorMessage: 'Error message will be here',
+    search: true,
+    disabled: true,
+  },
+}
