29f4e14e6f18fc168950a96cf021f78067da0645
nikolasmushroom <dashagribok0905@gmail.com>
2024-12-13 23:57:01 +0300

    feat: fix custom pagination, created utill function getpagesArray

---
+++ src/components/ui/pagination/getPagesArray.ts
@@ -0,0 +1,41 @@
+import { getRange } from './getRange.ts'
+
+export type getPagesArrayProps = {
+  currentPage: number
+  totalPagesCount: number
+  totalPageCount: number
+  siblingCount: number
+}
+export const getPagesArray = ({
+  currentPage,
+  totalPagesCount,
+  totalPageCount,
+  siblingCount,
+}: getPagesArrayProps) => {
+  return () => {
+    if (totalPagesCount >= totalPageCount) {
+      return getRange(1, totalPageCount)
+    }
+    const DOTS = '...'
+    const leftSiblingIndex = Math.max(Number(currentPage) - siblingCount, 1)
+    const rightSiblingIndex = Math.min(Number(currentPage) + siblingCount, totalPageCount)
+
+    const shouldShowLeftDots = leftSiblingIndex > 2
+    const shouldShowRightDots = rightSiblingIndex < totalPageCount - 2
+
+    if (!shouldShowLeftDots && shouldShowRightDots) {
+      let leftItemCount = 3 + 2 * siblingCount
+      let leftRange = getRange(1, leftItemCount)
+      return [...leftRange, DOTS, totalPageCount]
+    }
+    if (shouldShowLeftDots && !shouldShowRightDots) {
+      let rightItemCount = 3 + 2 * siblingCount
+      let rightRange = getRange(totalPageCount - rightItemCount + 1, totalPageCount)
+      return [1, DOTS, ...rightRange]
+    }
+    if (shouldShowLeftDots && shouldShowRightDots) {
+      let middleRange = getRange(leftSiblingIndex, rightSiblingIndex)
+      return [1, DOTS, ...middleRange, DOTS, totalPageCount]
+    }
+  }
+}
