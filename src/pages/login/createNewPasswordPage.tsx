1466dd14cfd63b00ee0155e91fe2df80f09dd4dd
nikolasmushroom <dashagribok0905@gmail.com>
2024-12-23 15:26:05 +0300

    fix: created createNewPasswordPage

---
+++ src/pages/login/createNewPasswordPage.tsx
@@ -0,0 +1,10 @@
+import { Page } from '@/components/ui/page'
+import { CreateNewPassword } from '@/components/auth/createNewPassword'
+
+export const CreateNewPasswordPage = () => {
+  return (
+    <Page>
+      <CreateNewPassword onSubmit={() => {}} />
+    </Page>
+  )
+}
