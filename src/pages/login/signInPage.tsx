94e2ce1563aa7e84a607b86b0fa47efdf619ad40
nikolasmushroom <dashagribok0905@gmail.com>
2024-12-28 23:29:55 +0300

    fix: added queries

--- src/pages/login/signInPage.tsx
+++ src/pages/login/signInPage.tsx
@@ -1,13 +1,24 @@
 import { Page } from '@/components/ui/page'
 import { SignIn } from '@/components/auth/signIn'
-import { useLoginMutation } from '@/services/auth/authService.ts'
-import { CreateNewAccount } from '@/services/auth/authServiceTypes.ts'
+import { useLoginMutation, useMeQuery } from '@/services/auth/auth.service.ts'
+import { CreateNewAccount } from '@/services/auth/auth.serviceTypes.ts'
+import { useNavigate } from 'react-router-dom'
+import { useEffect } from 'react'
 
 export const SignInPage = () => {
   const [login] = useLoginMutation()
-  const onSubmitHandler = ({ password, email }: CreateNewAccount) => {
-    login({ password, email })
+  const { data } = useMeQuery()
+  const navigate = useNavigate()
+
+  const onSubmitHandler = async ({ password, email }: CreateNewAccount) => {
+    await login({ password, email })
   }
+  useEffect(() => {
+    if (data) {
+      navigate('/')
+    }
+  }, [data])
+
   return (
     <Page>
       <SignIn onSubmit={onSubmitHandler} />
