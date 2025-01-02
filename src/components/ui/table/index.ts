1f87095e476b05d1e4c5ad60773e8e67a9636d71
nikolasmushroom <dashagribok0905@gmail.com>
2025-01-02 22:55:30 +0300

    fix: added export files

---
+++ src/components/ui/table/index.ts
@@ -0,0 +1,2 @@
+export * from './table-header.tsx'
+export * from './table.tsx'
---
+++ src/pages/login/index.ts
@@ -0,0 +1,5 @@
+export * from './createNewPasswordPage.tsx'
+export * from './forgotPasswordPage.tsx'
+export * from './signInPage.tsx'
+export * from './signUpPage.tsx'
+export * from './checkEmailPage.tsx'
