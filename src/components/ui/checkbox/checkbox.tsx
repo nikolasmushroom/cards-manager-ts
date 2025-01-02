78f03ba421291e6cc77c754de4f1668ed115444d
nikolasmushroom <dashagribok0905@gmail.com>
2025-01-02 22:36:19 +0300

    feat: wrapped with forwardRef

--- src/components/ui/checkbox/checkbox.tsx
+++ src/components/ui/checkbox/checkbox.tsx
@@ -1,7 +1,7 @@
 import * as RadixCheckbox from '@radix-ui/react-checkbox'
 import s from './checkbox.module.scss'
 import clsx from 'clsx'
-import { ComponentPropsWithoutRef } from 'react'
+import { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react'
 import { Typography } from '../typography'
 import CheckedIcon from '../../../common/icons/CheckedIcon.tsx'
 
@@ -10,48 +10,54 @@ export type CheckboxProps = {
   onCheckedChange?: (checked: boolean) => void
   position?: 'left' | 'right' | 'default'
 } & ComponentPropsWithoutRef<typeof RadixCheckbox.Root>
-export const Checkbox = ({
-  onCheckedChange,
-  checked,
-  defaultChecked,
-  id = 'c1',
-  disabled,
-  position = 'default',
-  label,
-  className,
-  required,
-  ...rest
-}: CheckboxProps) => {
-  const classNames = {
-    container: clsx(s.container, className),
-    checkboxWrapper: clsx(s.checkboxWrapper, disabled && s.disabled),
-    checkedIcon: clsx(s.checkedIcon, disabled && s.disabled),
-    label: clsx(s.Label, disabled && s.disabled),
-    indicator: clsx(s.Indicator),
-  }
-  return (
-    <div className={classNames.container}>
-      <div className={classNames.checkboxWrapper}>
-        <RadixCheckbox.Root
-          className={s.Root}
-          defaultChecked={defaultChecked}
-          onCheckedChange={onCheckedChange}
-          checked={checked}
-          disabled={disabled}
-          id={id}
-          {...rest}
-        >
-          <RadixCheckbox.Indicator className={classNames.indicator}>
-            {checked && <CheckedIcon className={classNames.checkedIcon} />}
-          </RadixCheckbox.Indicator>
-        </RadixCheckbox.Root>
-      </div>
+export const Checkbox = forwardRef<ElementRef<typeof RadixCheckbox.Root>, CheckboxProps>(
+  (
+    {
+      onCheckedChange,
+      checked,
+      defaultChecked,
+      id = 'c1',
+      disabled,
+      position = 'default',
+      label,
+      className,
+      required,
+      ...rest
+    },
+    ref
+  ) => {
+    const classNames = {
+      container: clsx(s.container, className),
+      checkboxWrapper: clsx(s.checkboxWrapper, disabled && s.disabled),
+      checkedIcon: clsx(s.checkedIcon, disabled && s.disabled),
+      label: clsx(s.Label, disabled && s.disabled),
+      indicator: clsx(s.Indicator),
+    }
+    return (
+      <div className={classNames.container}>
+        <div className={classNames.checkboxWrapper}>
+          <RadixCheckbox.Root
+            ref={ref}
+            className={s.Root}
+            defaultChecked={defaultChecked}
+            onCheckedChange={onCheckedChange}
+            checked={checked}
+            disabled={disabled}
+            id={id}
+            {...rest}
+          >
+            <RadixCheckbox.Indicator className={classNames.indicator}>
+              {checked && <CheckedIcon className={classNames.checkedIcon} />}
+            </RadixCheckbox.Indicator>
+          </RadixCheckbox.Root>
+        </div>
 
-      {label && (
-        <Typography as={'label'} variant={'Body2'} className={classNames.label} htmlFor={id}>
-          {label}
-        </Typography>
-      )}
-    </div>
-  )
-}
+        {label && (
+          <Typography as={'label'} variant={'Body2'} className={classNames.label} htmlFor={id}>
+            {label}
+          </Typography>
+        )}
+      </div>
+    )
+  }
+)
