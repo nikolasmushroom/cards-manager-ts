96a0ddd431a840c4b21fb1bdffaf4b4a504632f4
nikolasmushroom <dashagribok0905@gmail.com>
2025-01-02 22:30:14 +0300

    feat: added auth service and types for it

---
+++ src/services/auth/auth.service.ts
@@ -0,0 +1,92 @@
+import { baseApi } from '@/services/baseApi.ts'
+import {
+  AuthMeInterface,
+  CreateNewAccount,
+  EditProfileArgs,
+  LoginResponse,
+  MeResponse,
+  RecoveryPasswordArgs,
+  ResetPasswordArgs,
+} from '@/services/auth/auth.serviceTypes.ts'
+
+export const authService = baseApi.injectEndpoints({
+  endpoints: build => ({
+    me: build.query<AuthMeInterface, void>({
+      query: () => ({
+        url: '/v1/auth/me',
+        arguments: {},
+      }),
+      providesTags: ['Auth'],
+    }),
+    signUp: build.mutation<void, CreateNewAccount>({
+      query: args => ({
+        url: '/v1/auth/sign-up',
+        method: 'POST',
+        body: args,
+      }),
+    }),
+    login: build.mutation<LoginResponse, CreateNewAccount>({
+      async onQueryStarted(_, { queryFulfilled }) {
+        const { data } = await queryFulfilled
+        if (!data) {
+          return
+        }
+        localStorage.setItem('accessToken', data.accessToken)
+        localStorage.setItem('refreshToken', data.refreshToken)
+      },
+      query: args => ({
+        url: '/v1/auth/login',
+        method: 'POST',
+        body: args,
+      }),
+      invalidatesTags: ['Auth'],
+    }),
+    logout: build.mutation({
+      query: () => ({
+        url: '/v2/auth/logout',
+        method: 'POST',
+        arguments: {},
+      }),
+      async onQueryStarted(_, { queryFulfilled, dispatch }) {
+        dispatch(authService.util.resetApiState())
+        const { data } = await queryFulfilled
+        if (!data) {
+          return
+        }
+        localStorage.removeItem('refreshToken')
+        localStorage.removeItem('accessToken')
+      },
+      invalidatesTags: ['Auth'],
+    }),
+    editProfileInfo: build.mutation<MeResponse, EditProfileArgs>({
+      query: args => ({
+        url: '/v1/auth/me',
+        method: 'PATCH',
+        body: args,
+      }),
+    }),
+    recoveryPassword: build.mutation<MeResponse, RecoveryPasswordArgs>({
+      query: args => ({
+        url: '/v1/auth/recover-password',
+        method: 'POST',
+        body: args,
+      }),
+    }),
+    resetPassword: build.mutation<MeResponse, ResetPasswordArgs>({
+      query: ({ password, token }) => ({
+        url: `/v1/auth/reset-password/${token}`,
+        method: 'POST',
+        body: { password },
+      }),
+    }),
+  }),
+})
+export const {
+  useMeQuery,
+  useEditProfileInfoMutation,
+  useSignUpMutation,
+  useLoginMutation,
+  useLogoutMutation,
+  useRecoveryPasswordMutation,
+  useResetPasswordMutation,
+} = authService
---
+++ src/services/auth/auth.serviceTypes.ts
@@ -0,0 +1,44 @@
+export interface AuthMeInterface {
+  avatar: string
+  id: string
+  email: string
+  isEmailVerified: boolean
+  name: string
+  created: string
+  updated: string
+}
+
+export interface EditProfileArgs {
+  avatar: string
+  name: string
+}
+
+export interface MeResponse {
+  avatar: string
+  id: string
+  email: string
+  isEmailVerified: boolean
+  name: string
+  created: string
+  updated: string
+}
+
+export interface CreateNewAccount {
+  password: string
+  email: string
+}
+
+export interface LoginResponse {
+  accessToken: string
+  refreshToken: string
+}
+
+export interface RecoveryPasswordArgs {
+  email: string
+  html?: string
+  subject?: string
+}
+export interface ResetPasswordArgs {
+  password: string
+  token: string
+}
