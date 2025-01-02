bc3bf17ec812688d2518934f72faa31034cffd9e
nikolasmushroom <dashagribok0905@gmail.com>
2025-01-02 22:57:31 +0300

    feat:  added logout logic to layout component

--- src/components/ui/layout/layout.tsx
+++ src/components/ui/layout/layout.tsx
@@ -1,14 +1,26 @@
 import { ComponentProps, CSSProperties, ElementRef, forwardRef } from 'react'
 import s from './layout.module.scss'
 import { Header } from '@/components/ui'
-import { useMeQuery } from '@/services/auth/auth.service.ts'
+import { useLogoutMutation, useMeQuery } from '@/services/auth/auth.service.ts'
+import { Outlet, useNavigate } from 'react-router-dom'
 
 type Props = {
   contentMarginTop?: CSSProperties['marginTop']
 } & ComponentProps<'div'>
 export const Layout = forwardRef<ElementRef<'div'>, Props>(
   ({ contentMarginTop = '36px', children, ...rest }, ref) => {
-    const { data } = useMeQuery()
+    const { data, isLoading } = useMeQuery()
+    const [logout] = useLogoutMutation()
+
+    const navigate = useNavigate()
+    const logoutHandler = () => {
+      logout({}).then(() => {
+        navigate('/login')
+      })
+    }
+    if (isLoading) {
+      return <div>Loading...</div>
+    }
     return (
       <div ref={ref} {...rest} className={s.layout}>
         <Header
@@ -17,8 +29,11 @@ export const Layout = forwardRef<ElementRef<'div'>, Props>(
           userName={data?.name}
           shortName={data?.name}
           userEmail={data?.email}
+          logout={logoutHandler}
         />
-        <main className={s.main}>{children}</main>
+        <main className={s.main}>
+          <Outlet />
+        </main>
       </div>
     )
   }
