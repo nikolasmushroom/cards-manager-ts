dd4c8e761a664f038ecfdbb96b609f245acbdba8
nikolasmushroom <dashagribok0905@gmail.com>
2025-01-02 23:10:24 +0300

    feat: global changes

---
+++ .storybook/preview.ts
@@ -0,0 +1,30 @@
+import type { Preview } from '@storybook/react'
+import { themes } from '@storybook/theming'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/700.css'
+
+import '../src/styles/index.scss'
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    docs: {
+      theme: themes.dark,
+    },
+    backgrounds: {
+      default: 'dark',
+      values: [
+        {
+          name: 'dark',
+          value: '#000000',
+        },
+      ],
+    },
+  },
+}
+
+export default preview
