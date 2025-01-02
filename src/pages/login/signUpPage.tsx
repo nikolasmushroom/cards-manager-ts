bead45257870e7d751ae42ce39911a211ff4fc9f
nikolasmushroom <dashagribok0905@gmail.com>
2025-01-02 23:06:20 +0300

    fix: removed debuggers from sign-up page

--- src/pages/login/signUpPage.tsx
+++ src/pages/login/signUpPage.tsx
@@ -1,16 +1,14 @@
 import { Page } from '@/components/ui/page'
 import { SignUp } from '@/components/auth/signUp'
-import { useSignUpMutation } from '@/services/auth/authService.ts'
-import { CreateNewAccount } from '@/services/auth/authServiceTypes.ts'
+import { useSignUpMutation } from '@/services/auth/auth.service.ts'
+import { CreateNewAccount } from '@/services/auth/auth.serviceTypes.ts'
 import { useNavigate } from 'react-router-dom'
 
 export const SignUpPage = () => {
   const [signUp] = useSignUpMutation()
   const navigate = useNavigate()
   const onSubmitHandler = async ({ password, email }: CreateNewAccount) => {
-    debugger
     let res = await signUp({ password, email })
-    debugger
     if (res.data) {
       navigate('/login')
     }
