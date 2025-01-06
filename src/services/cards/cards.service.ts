04d22339ca6399f38f1fdfd470c030e0b9605598
nikolasmushroom <dashagribok0905@gmail.com>
2025-01-06 22:16:20 +0300

    feat: added optimistic updates to cards.service.ts

--- src/services/cards/cards.service.ts
+++ src/services/cards/cards.service.ts
@@ -22,18 +22,31 @@ export const cardsService = baseApi.injectEndpoints({
     getMinMaxCards: build.query<DeckMinMaxResponse, void>({
       query: () => 'v2/decks/min-max-cards',
     }),
-    createCard: build.mutation<CreateCardResponse, CreateCardArgs>({
-      query: args => {
-        const { id, ...rest } = args
-        return {
-          url: `v1/decks/${id}/cards`,
-          method: 'POST',
-          body: rest,
+    updateCard: build.mutation<CreateCardResponse, UpdateCardArgs>({
+      async onQueryStarted({ id, ...args }, { dispatch, getState, queryFulfilled }) {
+        const invalidatedBy = cardsService.util.selectInvalidatedBy(getState(), [
+          { type: 'DeckCards' },
+        ])
+        const patchResults: any[] = []
+        invalidatedBy.forEach(({ originalArgs }) => {
+          patchResults.push(
+            dispatch(
+              cardsService.util.updateQueryData('getDeckCards', originalArgs, draft => {
+                const itemToUpdateIndex = draft.items.findIndex(card => card.id === id)
+                if (itemToUpdateIndex === -1) {
+                  return
+                }
+                Object.assign(draft.items[itemToUpdateIndex], args)
+              })
+            )
+          )
+        })
+        try {
+          await queryFulfilled
+        } catch (e) {
+          patchResults.forEach(patchResult => patchResult.undo())
         }
       },
-      invalidatesTags: ['Cards', 'DeckCards'],
-    }),
-    updateCard: build.mutation<CreateCardResponse, UpdateCardArgs>({
       query: args => {
         const { id, ...rest } = args
         return {
@@ -42,23 +55,78 @@ export const cardsService = baseApi.injectEndpoints({
           body: rest,
         }
       },
-      invalidatesTags: ['Cards', 'DeckCards'],
+      invalidatesTags: ['DeckCards'],
+    }),
+    createCard: build.mutation<CreateCardResponse, CreateCardArgs>({
+      async onQueryStarted({}, { getState, dispatch, queryFulfilled }) {
+        const invalidatedBy = cardsService.util.selectInvalidatedBy(getState(), [
+          { type: 'DeckCards' },
+        ])
+        try {
+          const { data } = await queryFulfilled
+          invalidatedBy.forEach(({ originalArgs }) => {
+            dispatch(
+              cardsService.util.updateQueryData('getDeckCards', originalArgs, draft => {
+                if (originalArgs.currentPage !== 1) {
+                  return
+                }
+                draft.items.unshift({ ...data, grade: 0 })
+              })
+            )
+          })
+        } catch (e) {
+          console.log(e)
+        }
+      },
+      query: args => {
+        const { id, ...rest } = args
+        return {
+          url: `v1/decks/${id}/cards`,
+          method: 'POST',
+          body: rest,
+        }
+      },
+      invalidatesTags: ['DeckCards'],
     }),
     getCardById: build.query<void, DeleteCardArgs>({
       query: ({ id }) => ({
         url: `v1/cards/${id}/`,
         method: 'GET',
       }),
-      providesTags: ['Cards', 'DeckCards'],
+      providesTags: ['DeckCards'],
     }),
     deleteCard: build.mutation<void, DeleteCardArgs>({
+      async onQueryStarted({ id }, { dispatch, getState, queryFulfilled }) {
+        const invalidatedBy = cardsService.util.selectInvalidatedBy(getState(), [
+          { type: 'DeckCards' },
+        ])
+        const deleteResults: any[] = []
+        invalidatedBy.forEach(({ originalArgs }) => {
+          deleteResults.push(
+            dispatch(
+              cardsService.util.updateQueryData('getDeckCards', originalArgs, draft => {
+                const itemToUpdateIndex = draft.items.findIndex(card => card.id === id)
+                if (itemToUpdateIndex === -1) {
+                  return
+                }
+                draft.items.splice(itemToUpdateIndex, 1)
+              })
+            )
+          )
+        })
+        try {
+          await queryFulfilled
+        } catch (e) {
+          deleteResults.forEach(deleteResult => deleteResult.undo())
+        }
+      },
       query: ({ id }) => {
         return {
           url: `v1/cards/${id}/`,
           method: 'DELETE',
         }
       },
-      invalidatesTags: ['Cards', 'DeckCards'],
+      invalidatesTags: ['DeckCards'],
     }),
   }),
 })
