a61e8b343813e36ff2387735ac6d45870503706d
nikolasmushroom <dashagribok0905@gmail.com>
2025-01-06 22:16:56 +0300

    feat: added logic to create new password page

--- src/pages/login/createNewPasswordPage.tsx
+++ src/pages/login/createNewPasswordPage.tsx
@@ -1,14 +1,19 @@
 import { Page } from '@/components/ui/page'
 import { CreateNewPassword } from '@/components/auth/createNewPassword'
 import { useResetPasswordMutation } from '@/services/auth/auth.service.ts'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import { createNewPasswordForm } from '@/components/auth/createNewPassword/useCreateNewPassword.ts'
 
 export const CreateNewPasswordPage = () => {
   const { token } = useParams()
+  const navigate = useNavigate()
   const [resetPassword] = useResetPasswordMutation()
   const onSubmitHandler = (data: createNewPasswordForm) => {
     resetPassword({ password: data.password, token: token ?? '' })
+      .unwrap()
+      .then(() => {
+        navigate('/login')
+      })
   }
   return (
     <Page>
