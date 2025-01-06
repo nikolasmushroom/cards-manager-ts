2daa14d4e510fd51d314bdb0e57254da6d70811d
nikolasmushroom <dashagribok0905@gmail.com>
2025-01-06 17:10:01 +0300

    feat: updated icons container and icons

--- src/pages/decks-page/ui/decks-table/decks-table.tsx
+++ src/pages/decks-page/ui/decks-table/decks-table.tsx
@@ -6,25 +6,23 @@ import { useState } from 'react'
 import s from './decks-table.module.scss'
 import { Deck } from '@/services/decks/decks.types.ts'
 import StartIcon from '@/common/icons/StartIcon.tsx'
-import TrashCan from '@/common/icons/TrashCan.tsx'
-import { useDeleteDeckMutation } from '@/services/decks/decks.service.ts'
 import { useNavigate } from 'react-router-dom'
-import { UpdateDeckModal } from '@/components/deck/deck-dialog/update-deck-modal.tsx'
+import { UpdateDeckModal } from '@/components/deck/deck-modal/update-deck-modal.tsx'
+import { DeleteDeckModal } from '@/components/deck/deck-modal/delete-deck-modal.tsx'
 
 type Props = {
   currentUserId?: string
   decks?: Deck[]
 }
+const columns: Column[] = [
+  { key: '1', title: 'Name' },
+  { key: '2', title: 'Cards' },
+  { key: '3', title: 'Last Updated' },
+  { key: '4', title: 'Created by' },
+  { key: '5', title: '' },
+]
 export const DecksTable = ({ currentUserId, decks }: Props) => {
   const [sort, setSort] = useState<SortType>({ key: '3', direction: 'asc' })
-  const columns: Column[] = [
-    { key: '1', title: 'Name' },
-    { key: '2', title: 'Cards' },
-    { key: '3', title: 'Last Updated' },
-    { key: '4', title: 'Created by' },
-    { key: '5', title: '' },
-  ]
-  const [deleteDeck] = useDeleteDeckMutation()
   const navigate = useNavigate()
   const onNavigateTo = (id: string) => {
     navigate(`/decks/${id}`)
@@ -37,9 +35,7 @@ export const DecksTable = ({ currentUserId, decks }: Props) => {
           return (
             <TRow key={deck.id}>
               <TCell>
-                <Typography variant={'Body2'} onClick={() => onNavigateTo(deck.id)}>
-                  {deck.name}
-                </Typography>
+                <Typography variant={'Body2'}>{deck.name}</Typography>
               </TCell>
               <TCell>
                 <Typography variant={'Body2'}>{deck.cardsCount}</Typography>
@@ -54,16 +50,12 @@ export const DecksTable = ({ currentUserId, decks }: Props) => {
               </TCell>
               <TCell>
                 <div className={s.iconsContainer}>
-                  {deck.userId === currentUserId ? (
-                    <div className={s.iconsContainer}>
-                      <StartIcon />
+                  <StartIcon onClick={() => onNavigateTo(deck.id)} />
+                  {deck.userId === currentUserId && (
+                    <>
                       <UpdateDeckModal title={'Update Deck'} deck={deck} />
-                      <TrashCan onClick={() => deleteDeck({ id: deck.id })} />
-                    </div>
-                  ) : (
-                    <div className={s.iconsContainer}>
-                      <StartIcon />
-                    </div>
+                      <DeleteDeckModal deck={deck} />
+                    </>
                   )}
                 </div>
               </TCell>
