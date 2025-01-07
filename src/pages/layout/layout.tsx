7fc7fc0010356eb6b91f9dab794e140af1cde992
nikolasmushroom <dashagribok0905@gmail.com>
2025-01-07 23:54:25 +0300

    feat: added logout logic to layout

--- src/pages/layout/layout.tsx
+++ src/pages/layout/layout.tsx
@@ -3,6 +3,7 @@ import s from './layout.module.scss'
 import { Header } from '@/components/ui'
 import { useLogoutMutation, useMeQuery } from '@/services/auth/auth.service.ts'
 import { Outlet, useNavigate } from 'react-router-dom'
+import { Progress } from '@radix-ui/themes'
 
 type Props = {
   contentMarginTop?: CSSProperties['marginTop']
@@ -13,10 +14,15 @@ export const Layout = forwardRef<ElementRef<'div'>, Props>(
     const [logout] = useLogoutMutation()
 
     const navigate = useNavigate()
+    const navigateToLogin = () => {
+      navigate('/login')
+    }
     const logoutHandler = () => {
-      logout({}).then(() => {
-        navigate('/login')
-      })
+      logout({})
+        .unwrap()
+        .then(() => {
+          navigate('/login')
+        })
     }
     if (isLoading) {
       return <div>Loading...</div>
@@ -25,12 +31,14 @@ export const Layout = forwardRef<ElementRef<'div'>, Props>(
       <div ref={ref} {...rest} className={s.layout}>
         <Header
           isLoggedIn={!!data}
+          navigateToLogin={navigateToLogin}
           userPhoto={data?.avatar}
           userName={data?.name}
           shortName={data?.name}
           userEmail={data?.email}
           logout={logoutHandler}
         />
+        <Progress duration={`30s`} />
         <main className={s.main}>
           <Outlet />
         </main>
